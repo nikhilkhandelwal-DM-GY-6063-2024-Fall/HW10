accd3a98c13ad7513264714694f636710cfe2cd0
nikhil0119 <nikhilkhandelwal0119@gmail.com>
2024-11-28 21:56:39 -0500

    code updated

--- 0/sketch.js
+++ 0/sketch.js
@@ -1,12 +1,66 @@
+let song, amplitude, fft, colorPicker, slider, stars = [];
+
+function preload() {
+    song = loadSound('song.mp3');
+}
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+    createCanvas(windowWidth, windowHeight);
+    amplitude = new p5.Amplitude();
+    fft = new p5.FFT();
+    colorPicker = createColorPicker('#00ff00');
+    colorPicker.position(10, 10);
+    slider = createSlider(1, 10, 5);
+    slider.position(10, 40);
+    noStroke();
+
+    for (let i = 0; i < 150; i++) {
+        stars.push({
+            x: random(width),
+            y: random(height),
+            size: random(1, 3),
+            speed: random(0.5, 1.5),
+            brightness: random(50, 150),
+        });
+    }
+
+    if (!song.isPlaying()) {
+        song.loop(); // Start playing the song and keep it looping
+    }
 }
 
 function draw() {
-  background(255);
-  fill(0);
-  rect(width / 2.5, height / 4, width / 20, height / 2);
-  rect(width - width / 2.5 - width / 20, height / 4, width / 20, height / 2);
-  rect(width / 2.5 + width / 20, height / 4 - width / 20, width - 2 * width / 2.5 - 2 * width / 20, width / 20);
-  rect(width / 2.5 + width / 20, height / 4 + height / 2, width - 2 * width / 2.5 - 2 * width / 20, width / 20);
+    drawStarryBackground();
+
+    let volume = amplitude.getLevel();
+    let spectrum = fft.analyze();
+    let circleSize = map(volume, 0, 1, 50, 300);
+    let baseColor = colorPicker.color();
+    fill(red(baseColor), green(baseColor), blue(baseColor), 150);
+    ellipse(width / 2, height / 2, circleSize);
+
+    for (let i = 0; i < spectrum.length; i++) {
+        let angle = map(i, 0, spectrum.length, 0, TWO_PI);
+        let radius = map(spectrum[i], 0, 255, 100, width / 2);
+        let x = width / 2 + cos(angle) * radius;
+        let y = height / 2 + sin(angle) * radius;
+        fill(red(baseColor), green(baseColor), blue(baseColor), random(50, 200));
+        ellipse(x, y, slider.value());
+    }
+}
+
+function drawStarryBackground() {
+    background(0, 50);
+
+    let volume = amplitude.getLevel();
+    for (let star of stars) {
+        fill(255, star.brightness * map(volume, 0, 1, 0.5, 2));
+        ellipse(star.x, star.y, star.size);
+
+        star.y += star.speed;
+        if (star.y > height) {
+            star.y = 0;
+            star.x = random(width);
+        }
+    }
 }
--- 1/sketch.js
+++ 1/sketch.js
@@ -1,11 +1,48 @@
+let song;
+let peaks;
+let canvasHeight;
+let playButton;
+
+function preload() {
+  song = loadSound('songb.mp3');
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  canvasHeight = height / 2;
+  peaks = song.getPeaks(floor(5 * width));
+  playButton = createButton('Play/Pause');
+  playButton.position(10, 10);
+  playButton.mousePressed(togglePlay);
 }
 
 function draw() {
-  background(255);
-  fill(0);
-  rect(width / 2 - width / 40, height / 4 - width / 20, width / 20, height / 2 + width / 10);
-  rect(width / 2.5, height / 4 + height / 2, width - 2 * width / 2.5, width / 20);
-  rect(width / 2 - width / 20 - width / 40, height / 4 - width / 20, width / 20, width / 20);
+  background(20);
+  noFill();
+  strokeWeight(2);
+
+  beginShape();
+  for (let i = 0; i < peaks.length; i++) {
+    let x = map(i, 0, peaks.length, 0, width);
+    let y = canvasHeight + peaks[i] * canvasHeight;
+    stroke(255, 100, 150, 200);
+    vertex(x, y);
+  }
+  endShape();
+
+  noStroke();
+  fill(255);
+  textSize(20);
+  textAlign(CENTER, CENTER);
+  text("Static Visualization of Song Peaks", width / 2, 50);
+  textSize(14);
+  text("Amplitude data mapped to a waveform", width / 2, 80);
+}
+
+function togglePlay() {
+  if (song.isPlaying()) {
+    song.pause();
+  } else {
+    song.play();
+  }
 }
